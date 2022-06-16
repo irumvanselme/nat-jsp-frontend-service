49efcaf9b544ea83bf88bc21326e29861eb1847d
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2022-06-16 13:07:37 +0200

    feat: add the JSP Rendering Feature

--- src/main/java/rw/ac/rca/nat2022/frontend/FrontendServiceApplication.java
+++ src/main/java/rw/ac/rca/nat2022/frontend/FrontendServiceApplication.java
@@ -2,12 +2,18 @@ package rw.ac.rca.nat2022.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FrontendServiceApplication {
+public class FrontendServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FrontendServiceApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FrontendServiceApplication.class);
+    }
 }
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anselme
+  Date: 16/06/2022
+  Time: 13:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>My Frontend Application</title>
+</head>
+<body>
+<h1>The Frontend Application.</h1>
+</body>
+</html>
